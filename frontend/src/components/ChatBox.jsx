9e6ff25a504fc8e018a59a6726f2dcb47ad90533
Ernz Arellano <ernza@regalcredit.com>
2026-05-13 09:16:13 +0800

    add prompt input box

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -9,6 +9,14 @@ const Chatbox = () => {
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const [prompt, setPrompt] = useState("");
+  const [mode, setMode] = useState("text");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     if (selectedChat) {
       setMessages(selectedChat.messages);
@@ -37,10 +45,50 @@ const Chatbox = () => {
         ))}
 
         {/* Three Dots Loading */}
+        {loading && (
+          <div className="loader flex items-center gap-1.5">
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+          </div>
+        )}
       </div>
 
       {/*Prompt Input Box */}
-      <form action=""></form>
+      <form
+        onSubmit={onSubmit}
+        className="bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center"
+        action=""
+      >
+        <select
+          onChange={(e) => setMode(e.target.value)}
+          value={mode}
+          className="text-sm pl-3 pr-2 outline-none"
+          name=""
+          id=""
+        >
+          <option className="dark:bg-purple-900" value="text">
+            Text
+          </option>
+          <option className="dark:bg-purple-900" value="image">
+            Image
+          </option>
+        </select>
+        <input
+          onChange={(e) => setPrompt(e.target.value)}
+          value={prompt}
+          type="text"
+          placeholder="Type your prompt here..."
+          className="flex-1 w-full text-sm outline-none required"
+        />
+        <button disabled={loading}>
+          <img
+            src={loading ? assets.stop_icon : assets.send_icon}
+            className="w-8 cursor-pointer"
+            alt=""
+          />
+        </button>
+      </form>
     </div>
   );
 };
