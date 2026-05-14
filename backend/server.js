9af9bb22e71a3804e90e9218c73dd159be4f98f8
Ernz Arellano <ernza@regalcredit.com>
2026-05-14 12:04:33 +0800

    add user routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 
+import userRouter from "./routes/userRoutes.js";
+
 const app = express();
 
 await connectDB();
@@ -15,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is Live!");
 });
+
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
