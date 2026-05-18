3d7fd67f2861a50c8493f967a4de15afb88c9443
Ernz Arellano <ernza@regalcredit.com>
2026-05-18 14:12:30 +0800

    bugfix is paid not reflecting

--- backend/configs/webhooks.js
+++ backend/configs/webhooks.js
@@ -27,6 +27,7 @@ export const stripeWebhooks = async (req, res) => {
         });
 
         const session = sessionList.data[0];
+        console.log("Session Metadata:", JSON.stringify(session.metadata));
         const { transactionid, appId } = session.metadata;
 
         if (appId === "hackgpt") {
@@ -34,6 +35,11 @@ export const stripeWebhooks = async (req, res) => {
             _id: transactionid,
             isPaid: false,
           });
+
+          if (!transaction) {
+            console.log("Transaction not found or already paid");
+            return res.json({ received: true });
+          }
 
           // Update credits in user account
           await User.updateOne(
