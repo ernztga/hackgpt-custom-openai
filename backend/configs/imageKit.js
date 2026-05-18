3649c25dc1b0b0df29f25208e98503b8f261ac4b
Ernz Arellano <ernza@regalcredit.com>
2026-05-18 10:45:23 +0800

    add transaction controller

--- backend/configs/imageKit.js
+++ backend/configs/imageKit.js
@@ -4,11 +4,4 @@ const imagekit = new ImageKit({
   privateKey: process.env["IMAGEKIT_PRIVATE_KEY"], // This is the default and can be omitted
 });
 
-// const response = await imagekit.files.upload({
-//   file: fs.createReadStream("path/to/file"),
-//   fileName: "file-name.jpg",
-// });
-
-// console.log(response);
-
 export default imagekit;
