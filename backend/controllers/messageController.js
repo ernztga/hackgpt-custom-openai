62c590cddf75b55eae72edc2155da4f4e897a4c7
Ernz Arellano <ernza@regalcredit.com>
2026-05-18 10:33:11 +0800

    generate image using imagekit

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,8 @@
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
-import { openai } from "../config/openaiConfig.js";
+import openai from "../configs/openai.js";
 import axios from "axios";
+import imagekit from "../configs/imageKit.js";
 
 // Text-based AI Chat Message Controller
 export const textMessageController = async (req, res) => {
@@ -13,7 +14,7 @@ export const textMessageController = async (req, res) => {
         message: "You don't have enough credits to use this feature",
       });
     }
-    
+
     const { chatId, prompt } = req.body;
     const userId = req.user._id;
 
@@ -26,7 +27,7 @@ export const textMessageController = async (req, res) => {
     }
 
     // Call Gemini API to get AI response
-    const choices = await openai.chat.completions.create({
+    const response = await openai.chat.completions.create({
       model: "gemini-3-flash-preview",
       messages: [
         {
@@ -37,7 +38,7 @@ export const textMessageController = async (req, res) => {
     });
 
     const reply = {
-      ...choices[0].message,
+      ...response.choices[0].message,
       timestamp: Date.now(),
       isImage: false,
     };
@@ -78,13 +79,6 @@ export const imageMessageController = async (req, res) => {
         .json({ success: false, message: "Chat not found for user" });
     }
 
-    const reply = {
-      ...choices[0],
-      timestamp: Date.now(),
-      isImage: true,
-    };
-    res.json({ success: true, reply });
-
     // Add the new message to the chat's messages array
     chat.messages.push({
       role: "user",
@@ -107,7 +101,7 @@ export const imageMessageController = async (req, res) => {
     const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString("base64")}`;
 
     // Upload to Imagekit Media Library
-    const uploadResponse = await imagekit.upload({
+    const uploadResponse = await imagekit.files.upload({
       file: base64Image,
       fileName: `${Date.now()}.png`,
       folder: "hackgpt",
