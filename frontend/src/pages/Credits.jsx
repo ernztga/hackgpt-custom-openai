e6dbeff9c7e4f4dcaadcfce5d2a84861583b6699
Ernz Arellano <ernza@regalcredit.com>
2026-05-18 18:01:54 +0800

    add stripe payment to frontend

--- frontend/src/pages/Credits.jsx
+++ frontend/src/pages/Credits.jsx
@@ -2,14 +2,47 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { dummyPlans } from "../assets/assets";
 import Loading from "./Loading";
+import toast from "react-hot-toast";
+import { useAppContext } from "../context/AppContext";
 
 const Credits = () => {
   const [plans, setPlans] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { token, axios } = useAppContext();
 
-  const fetchPlans = () => {
-    setPlans(dummyPlans);
+  const fetchPlans = async () => {
+    try {
+      const { data } = await axios.get("/api/credit/plan", {
+        headers: { Authorization: token },
+      });
+
+      if (data.success) {
+        setPlans(data.plans);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message || "Failed to fetch plans");
+    }
     setLoading(false);
+  };
+
+  const purchasePlan = async (planId) => {
+    try {
+      const { data } = await axios.post(
+        "/api/credit/purchase",
+        { planId },
+        { headers: { Authorization: token } },
+      );
+
+      if (data.success) {
+        window.location.href = data.url;
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message || "Failed to initiate purchase");
+    }
   };
 
   useEffect(() => {
@@ -51,7 +84,14 @@ const Credits = () => {
               </ul>
             </div>
 
-            <button className="mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer">
+            <button
+              onClick={() =>
+                toast.promise(purchasePlan(plan._id), {
+                  loading: "Processing...",
+                })
+              }
+              className="mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer"
+            >
               Buy Now
             </button>
           </div>
